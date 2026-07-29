d97338fa18cba8799300ff9df7db53f1f44337d2
wncdev1 <cristean.yesman@wnc.com.br>
2026-07-29 12:35:44 -0300

    Initial Project

---
+++ credit-engine-api/src/test/java/com/srm/credit_engine_api/ProgramTests.java
@@ -0,0 +1,13 @@
+package com.srm.credit_engine_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
